a0bb24cc79f80e80605bd3e6e9b7b909deac7ba4
larafdezmorena <larafdezmorena@gmail.com>
2023-12-09 10:59:44 +0100

    add js function attempts

--- js/main.js
+++ js/main.js
@@ -6,13 +6,18 @@ const inputElement=document.querySelector('.js__input');
 const clueElement=document.querySelector('.js__clue');
 const triedElement=document.querySelector('.js__tried');
 
+//inicio número aleatorua y contador
+
+const randomNumber=getRandomNumber(100);
+let attempts = 0;
+
+console.log(randomNumber);
+
 //función para el número aleatorio
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
    };
- 
-   const randomNumber=getRandomNumber(100);
 
    console.log(randomNumber);
 
@@ -20,8 +25,10 @@ function getRandomNumber(max) {
 
 function showClue() {
     const number=parseInt(inputElement.value);
+    attempts++;
+
         if (number===randomNumber) {
-            clueElement.innerHTML = '¡Has acertado! ¡Enhorabuena! ;)';
+            clueElement.innerHTML = '¡Has acertado en '+attempts+' intentos! ¡Enhorabuena! ;)';
         }else if (number > 100) {
             clueElement.innerHTML = 'Tienes que elegir un número entre 1 y 100';
         }else if (number > randomNumber){
@@ -29,8 +36,13 @@ function showClue() {
         }else if (number < randomNumber) {
             clueElement.innerHTML = '¡Prueba con un número más alto! XD';
         }
+        triedElement.innerHTML = 'Intentos:' + attempts;
 };
+
+//número de intentos
+
+//triedElement.innerHTML = 'Intentos: ${attempts}';
+triedElement.innerHTML = 'Intentos:' + attempts;
+
+//listener
 buttonElement.addEventListener('click' , showClue);
-
-//función para intentos
-
